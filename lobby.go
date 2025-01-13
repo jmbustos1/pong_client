666b0f6df0a078984dd1884eff254febef8ed476
jmbustos1 <josebm78@gmail.com>
2025-01-13 00:00:00 -0300

    lobby send message

--- lobby.go
+++ lobby.go
@@ -1,5 +1,7 @@
 package main
 
+import "log"
+
 type Lobby struct {
 	ID   string
 	Name string
@@ -11,4 +13,14 @@ func (g *Game) createLobby(lobbyName string) {
 		"player_id":  g.playerID,
 		"lobby_name": lobbyName,
 	})
+}
+
+func (g *Game) leaveLobby() {
+	g.client.SendMessage(map[string]interface{}{
+		"event":     "leave_lobby",
+		"player_id": g.playerID,
+		"lobby_id":  g.currentLobbyID, // Asegúrate de tener el ID del lobby actual
+	})
+	g.currentLobbyID = "" // Limpia el ID del lobby actual
+	log.Println("Saliendo del lobby...")
 }
